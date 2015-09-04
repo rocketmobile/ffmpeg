6746aafa47a945ba8e004da088fa1512bcde391b
pierce-h <piercewharmon@gmail.com>
2015-09-04 10:09:38 -0500

    wip

--- spec/ffmpeg_spec.rb
+++ spec/ffmpeg_spec.rb
@@ -6,7 +6,7 @@ describe Paperclip::Ffmpeg do
 
   describe "thumbnail" do
     it "does some stuff" do
-      video = Video.new(asset: iphone_video)
+      video = Video.new(asset: [android_video, iphone_video].sample)
       video.save
     end
   end
